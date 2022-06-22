835dd770c52a5057ac8fafd8fa9d5baa6ea2c4d5
dudubarbaro <eduardobarbaro.ifc@gmail.com>
2022-06-22 08:48:27 -0300

    projeto livraria

---
+++ src/components/template/FooterComp.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer>Rodapé</footer>
+</template>
+<style>
+footer {
+  background-color: #14213D;
+  height: 15%;
+  font-size: 1.3rem;
+  padding-left: 2rem;
+  color: #FCA311;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
